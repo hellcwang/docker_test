43f5cf32d558eb9c2209c0bee108783be603d6f2
hellcwang <45849373+hellcwang@users.noreply.github.com>
2021-05-23 19:51:37 +0800

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,4 +1,4 @@
-git pull https://github.com/hellcwang/docker_test.git
-cd docker
+git clone https://github.com/hellcwang/docker_test.git
+cd docker_test
 docker build -t test .
 docker run -p 8888:8888 test
